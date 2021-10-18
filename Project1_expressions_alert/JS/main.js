637985af18eb37309d9a5e677cf0fb4b4686b9b3
KMC1994 <90338657+KMC1994@users.noreply.github.com>
2021-10-18 12:19:35 +0100

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World!"); //displaying the window alert method
+document.write("What day is it today?"); //displaying the document write method
+var txt1 = "Today is "; //defining a variable and giving it a string value
+var txt2 = "Monday"; //defining a second variable and giving it a string value
+var txt1 = txt1.fontcolor("navy"); //adding a fontcolor to the str variable
+var txt2 = txt2.fontcolor("blue"); //adding a fontcolor to the second str variable
+document.write(txt1 + txt2); //concatening two string variables
+var x = 202.1; //defining a numerical variable
+document.write (x * 10); //writing an expression using the numerical variable
